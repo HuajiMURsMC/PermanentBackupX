973ed503616e1024fb714f7dfdb46b7e2a61335a
HuajiMUR233 <huajimur@jvav.me>
2022-11-25 21:10:21 +0800

    大概是强迫症吧

--- permanent_backup_x/config.py
+++ permanent_backup_x/config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2022  Huaji_MUR233
+# Copyright (C) 2022 Huaji_MUR233
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
